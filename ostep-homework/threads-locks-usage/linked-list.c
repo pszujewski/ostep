810cdd1208f325a0c975e1e1657efed994ecb5c0
Peter Szujewski <pszujewski@gmail.com>
2022-07-25 09:16:30 -0400

    ll implementation

--- ostep-homework/threads-locks-usage/linked-list.c
+++ ostep-homework/threads-locks-usage/linked-list.c
@@ -3,24 +3,47 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <assert.h>
 
 typedef struct __list_node_t
 {
     int index;
-    ListNode *next;
-    void *data
+    struct __list_node_t *next;
+    void *data;
 } ListNode;
 
-void main()
+void List_Init(ListNode *list);
+int List_Insert(ListNode *list, void *data);
+
+void List_Free(ListNode *list);
+ListNode *List_Get(ListNode *node, int index);
+
+void Test_Insert(ListNode *list, int data);
+
+int main()
 {
-    ListNode *root = malloc(sizeof(ListNode));
-    List_Insert(root, (void *)100);
-    List_Insert(root, (void *)99);
-    List_Insert(root, (void *)78);
-    List_Insert(root, (void *)42);
-    List_Insert(root, (void *)22);
-    assert(List_Get(root, 3)->data == 42);
+    ListNode *root = (ListNode *)malloc(sizeof(ListNode));
+
+    Test_Insert(root, 99);
+    Test_Insert(root, 78);
+    Test_Insert(root, 42);
+    Test_Insert(root, 22);
+    Test_Insert(root, 100);
+
+    int *data = (int *)(List_Get(root, 3)->data);
+    assert(*data == 22);
     List_Free(root);
+}
+
+void Test_Insert(ListNode *list, int data)
+{
+    int *addr = (int *)malloc(sizeof(int)); // int *addr is an address (pointer) to a "int" value in memory.
+    *addr = data;                           // "*" here is an operator saying "addr" is a variable of type "int pointer". Access the value at that pointer. So "addr" is a pointer var and "*addr" is an int.
+
+    int idx = List_Insert(list, (void *)addr);
+    int *foundaddr = (int *)(List_Get(list, idx)->data);
+
+    assert(*foundaddr == data);
 }
 
 void List_Init(ListNode *list)
@@ -30,31 +53,32 @@ void List_Init(ListNode *list)
     list->data = NULL;
 }
 
-int List_Insert(ListNode *list, void *data)
+int List_Insert(ListNode *list, void *data_address)
 {
     int index = 0;
 
     if (list->data == NULL)
     {
-        list->data = data;
+        list->data = data_address;
     }
     else
     {
-        ListNode *child = list->next;
+        ListNode *curr = list;
+        index++;
 
-        while (child != NULL)
+        while (curr->next != NULL)
         {
-            child = child->next;
+            curr = curr->next;
             index++;
         }
 
-        ListNode *node = malloc(sizeof(ListNode));
+        ListNode *node = (ListNode *)malloc(sizeof(ListNode));
         List_Init(node);
 
-        node->data = data;
+        node->data = data_address;
         node->index = index;
 
-        child = node;
+        curr->next = node;
     }
     return index;
 }
@@ -82,6 +106,8 @@ void List_Free(ListNode *list)
     }
 
     ListNode *next = list->next;
+
+    free(list->data);
     free(list);
 
     List_Free(next);
