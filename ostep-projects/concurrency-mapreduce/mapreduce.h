887cd9214e5a3ca08fa29f2331d8ea606f3ff12e
Peter Szujewski <pszujewski@gmail.com>
2023-01-25 07:51:35 -0500

    adds test prgm

--- ostep-projects/concurrency-mapreduce/mapreduce.h
+++ ostep-projects/concurrency-mapreduce/mapreduce.h
@@ -12,9 +12,31 @@ void MR_Emit(char *key, char *value);
 
 unsigned long MR_DefaultHashPartition(char *key, int num_partitions);
 
-void MR_Run(int argc, char *argv[], 
-	    Mapper map, int num_mappers, 
-	    Reducer reduce, int num_reducers, 
-	    Partitioner partition);
+void MR_Run(int argc, char *argv[],
+			Mapper map, int num_mappers,
+			Reducer reduce, int num_reducers,
+			Partitioner partition);
+
+unsigned long MR_DefaultHashPartition(char *key, int num_partitions)
+{
+	unsigned long hash = 5381;
+	int c;
+	while ((c = *key++) != '\0')
+		hash = hash * 33 + c;
+	return hash % num_partitions;
+}
+
+void MR_Emit(char *key, char *value)
+{
+	return;
+}
+
+void MR_Run(int argc, char *argv[],
+			Mapper map, int num_mappers,
+			Reducer reduce, int num_reducers,
+			Partitioner partition)
+{
+	return;
+}
 
 #endif // __mapreduce_h__
